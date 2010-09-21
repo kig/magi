68dd2846348f6ac4467bc09f84af66ebda18a4ba
kig <ilmari.heikkinen@gmail.com>
2010-09-21 03:47:16 +0300

    minify.sh: only generate magi.js

--- minify.sh
+++ minify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd src &&
-cat {matrix,gl_util,scenegraph,scene_util}.js | yui-compressor --type js > magi.base.min.js &&
-echo "Created magi.base.min.js";
+#cat {matrix,gl_util,scenegraph,scene_util}.js | yui-compressor --type js > magi.base.min.js &&
+#echo "Created magi.base.min.js";
 
-cat {tar,obj_loader,bin_loader}.js | yui-compressor --type js > magi.loaders.min.js &&
-echo "Created magi.loaders.min.js";
+#cat {tar,obj_loader,bin_loader}.js | yui-compressor --type js > magi.loaders.min.js &&
+#echo "Created magi.loaders.min.js";
 
 cat {matrix,gl_util,scenegraph,scene_util,tar,obj_loader,bin_loader}.js | yui-compressor --type js > magi.js &&
 echo "Created magi.js";
