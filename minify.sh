2910b2e5e3c9cd6c535815cf8161bbce78f45436
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2010-10-25 10:24:32 +0300

    minify.sh: clean up

--- minify.sh
+++ minify.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-cd src &&
-#cat {matrix,gl_util,scenegraph,scene_util}.js | yui-compressor --type js > magi.base.min.js &&
-#echo "Created magi.base.min.js";
-
-#cat {tar,obj_loader,bin_loader}.js | yui-compressor --type js > magi.loaders.min.js &&
-#echo "Created magi.loaders.min.js";
-
-cat {matrix,util,gl_util,scenegraph,scene_util,tar,obj_loader,bin_loader}.js | yui-compressor --type js > magi.js &&
+cat src/{matrix,util,gl_util,scenegraph,scene_util,tar,obj_loader,bin_loader}.js | yui-compressor --type js > src/magi.js &&
 echo "Created src/magi.js";
 
-cat ../slides/slides.js | yui-compressor --type js > ../slides/slides.base.min.js &&
+cat slides/slides.js | yui-compressor --type js > slides/slides.base.min.js &&
 echo "Created slides/slides.base.min.js";
 
